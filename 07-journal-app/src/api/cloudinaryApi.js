077401aaee5ee387841683bf1d239280f90f7c6e
Ezequiel Pasillas Gonzalez <153935168+EzequielPasillasGonzalez@users.noreply.github.com>
2024-06-03 23:25:12 -0600

    Delete 07-journal-app/src/api directory

--- 07-journal-app/src/api/cloudinaryApi.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const cloudinaryApi = axios.create({ //? asi ya se pueden hacer peticiones http
-    baseURL: "https://api.cloudinary.com/v1_1/dsewcra3v/image/upload"
-})
-
-export default cloudinaryApi;
